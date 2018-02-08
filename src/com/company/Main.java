2f1b43541fd5eb8d43be431c72a515c47b16597e
AlvaroPupo <juanhernandezpupo@gmail.com>
2018-02-08 14:13:01 -0500

    added menu and solved inputMismatchException

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,44 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+    /*
+    add game
+    remove game
+    check out game
+    view library
+    check in game
+    view checked out games
+    exit
 
+    add game
+    * scanner
+    * store name info
+    * prompt user when game is added with game name
+    * prompt user to add game by title
+
+    remove game
+    * remove from library
+    * prompt user game has been removed
+    * if the library is empty, prompt user that is empty, return to menu
+    * list of the available games
+
+    checkout
+    * prompt user on how to check out the game
+    * list games in library
+    * inform user if there are no games
+
+    view library / check out
+    * display games
+    * go directly to the menu
+
+    check in games
+    * get the list of the games checked out
+    * remove from check out
+    * add checked in games back to library
+    * prompt if there are no games to check
+     */
+
+    Menu menu = new Menu();
+    menu.startMenu();
     }
 }
---
+++ src/com/company/Menu.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+
+    private Scanner input = new Scanner(System.in);
+
+    //menu to prompt user for library options
+    public void startMenu () {
+
+        System.out.println("Welcome to the Video Game Library, what would you like to do? \n" +
+                "1. Add a game to the library\n" +
+                "2. Remove a game from the library \n" +
+                "3. View what is currently in the library \n" +
+                "4. check a game out \n" +
+                "5. check a game in \n" +
+                "6. view checked out games \n" +
+                "7. exit the program");
+
+        try {
+
+            switch (input.nextInt()) {
+                case 1:
+                    //add game
+                    break;
+                case 2:
+                    //remove game
+                    break;
+                case 3:
+                    //view library
+                    break;
+                case 4:
+                    //check out the game
+                    break;
+                case 5:
+                    //check in game
+                    break;
+                case 6:
+                    //view the checked out games
+                    break;
+                case 7:
+                    //exit program
+                    break;
+                default:
+                    // prompt the user to pick a correct number, loop back to menu
+                    break;
+            }
+        } catch (InputMismatchException ime) {
+            input.nextLine();
+            System.out.println("select a number from 1 to 7");
+            startMenu();
+
+        }
+    }
+}
