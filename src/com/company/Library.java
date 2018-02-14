fe5c859f6437ead8cb2894e88d9c2743943fb7c5
AlvaroPupo <juanhernandezpupo@gmail.com>
2018-02-14 15:49:34 -0500

    done

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -16,7 +16,6 @@ public class Library {
     private String firstGame;
     private String firstGame2;
     private String secondGame;
-    private String secondGame2;
     private String option;
     private String add;
     Calendar calendar = Calendar.getInstance();
@@ -26,84 +25,97 @@ public class Library {
         this.menu = menu;
     }
 
-    public void addGame () {
+    public void addGame() {
 
         System.out.println("What is the name of the game you would like to add?");
         firstGame = input.nextLine();
 
-
         Games.add(firstGame);
 
-        System.out.println("you added " + firstGame + " to your library");
-
-        System.out.println("Do you want to add another game? y / n");
-        add = input.nextLine();
-
-        if (add.equalsIgnoreCase("y")) {
-            System.out.println("What is the name of the game you would like to add?");
-            secondGame = input.nextLine();
-            Games.add(secondGame);
-            System.out.println("you added " + secondGame + " to your library");
-            System.out.println();
-            menu.startMenu();
-
-        } else if (add.equalsIgnoreCase("n")){
-            System.out.println();
-            menu.startMenu();
-        }
+        System.out.println("you added " + firstGame + " to your library on: " + dateFormat.format(calendar.getTime()));
+        System.out.println();
+        menu.startMenu();
         }
 
-        public void Gamelist () {
+    public void Gamelist() {
 
+
+        if (Games.isEmpty()){
+            System.out.println("There are no games in your library");
+            System.out.println();
+            menu.startMenu();
+        } else {
             System.out.println("Here are the games that you've saved on your library: ");
-            Gamelist2();
-            System.out.println();
         }
-        public void multipleOption () {
+        Gamelist2();
+        System.out.println();
 
-            System.out.println("what would you like to do? \n1. delete game\n2. Check in game\n3. Check out game\n4. go to the main menu");
-            option = input.nextLine();
-        if (option.equalsIgnoreCase("1")){
+    }
+
+    public void multipleOption() {
+
+        System.out.println("what would you like to do? \n1. delete game\n2. Check in game\n3. Check out game\n4. go to the main menu");
+        option = input.nextLine();
+        if (option.equalsIgnoreCase("1")) {
             System.out.println();
             deleteGame();
-        }else if (option.equalsIgnoreCase("2")){
+        } else if (option.equalsIgnoreCase("2")) {
             System.out.println();
             checkedInGames();
-        }else if (option.equalsIgnoreCase("3")){
+        } else if (option.equalsIgnoreCase("3")) {
             System.out.println();
             checkOut();
-        }else if (option.equalsIgnoreCase("4")){
+        } else if (option.equalsIgnoreCase("4")) {
             System.out.println();
             menu.startMenu();
         }
     }
-        public void deleteGame() {
 
-            System.out.println("which game would you like to delete");
-            int uno = 1;
-            String gamesOnTheLibrary[] = new String[2];
-            gamesOnTheLibrary[0] = firstGame;
-            gamesOnTheLibrary[1] = secondGame;
-            for (int i = 0; i <gamesOnTheLibrary.length; i++) {
-                System.out.println(uno + ". " + gamesOnTheLibrary[i]);
-                uno++;
-            }
-            input.nextInt();
-            Games.remove(gamesOnTheLibrary);
-            System.out.println("Game deleted.");
+    public void deleteGame() {
+
+
+        if (Games.isEmpty()){
+            System.out.println("There are no games to delete");
             System.out.println();
             menu.startMenu();
+        }else{
+            System.out.println("which game would you like to delete");
         }
+        int index = 1;
+        for (String game : Games) {
+
+            System.out.println(index++ + ". " + game);
+        }
+        if (index == 1) {
+
+        }
+        firstGame = input.nextLine();
+
+        Games.remove(firstGame);
+
+//            Games.remove(gamesOnTheLibrary);
+            System.out.println(firstGame +  " deleted.");
+            System.out.println();
+            menu.startMenu();
+
+}
         public void checkOut() {
 
-            System.out.println("Which game would you like to check out: ");
-            int uno = 1;
-            String gamesOnTheLibrary[] = new String[2];
-            gamesOnTheLibrary[0] = firstGame;
-            gamesOnTheLibrary[1] = secondGame;
-            for (int i = 0; i <gamesOnTheLibrary.length; i++) {
-                System.out.println(uno + ". " + gamesOnTheLibrary[i]);
-                uno++;
+
+            if (Games.isEmpty()){
+                System.out.println("There are no games in your library to check out");
+                System.out.println();
+                menu.startMenu();
+            }else{
+                System.out.println("Which game would you like to check out: ");
+            }
+            int index = 1;
+            for (String game : Games) {
+
+                System.out.println(index++ + ". " + game);
+            }
+            if (index == 1) {
+
             }
             System.out.println("(to select, please enter the name of the game)");
             firstGame2 = input.nextLine();
@@ -112,13 +124,14 @@ public class Library {
         }
         public void Gamelist2 (){
 
-            int uno = 1;
-            String gamesOnTheLibrary[] = new String[2];
-            gamesOnTheLibrary[0] = firstGame;
-            gamesOnTheLibrary[1] = secondGame;
-            for (int i = 0; i <gamesOnTheLibrary.length; i++) {
-                System.out.println(uno + ". " + gamesOnTheLibrary[i]);
-                uno++;
+
+            int index = 1;
+            for (String game : Games) {
+
+                System.out.println(index++ + ". " + game);
+            }
+            if (index == 1) {
+
             }
             multipleOption();
 
@@ -155,14 +168,20 @@ public class Library {
         }
         public void CheckedOutList () {
 
-            System.out.println("Here are the list of the games you have checked out: ");
-            int uno = 1;
-            String gamesCheckedOut[] = new String[2];
-            gamesCheckedOut[0] = firstGame2;
-            gamesCheckedOut[1] = secondGame2;
-            for (int i = 0; i <gamesCheckedOut.length; i++){
-                System.out.println(uno + ". " + gamesCheckedOut[i]);
-                uno++;
+            if (checkedOutGames.isEmpty()){
+                System.out.println("There are no checked out games in your library");
+                System.out.println();
+                menu.startMenu();
+            }else {
+                System.out.println("Here are the list of the games you have checked out: ");
+            }
+            int index = 1;
+            for (String game : checkedOutGames) {
+
+                System.out.println(index++ + ". " + game);
+            }
+            if (index == 1) {
+
             }
             System.out.println();
             System.out.println("1. Check in a game\n2. go to main menu");
